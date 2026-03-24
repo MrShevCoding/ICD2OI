40eccb90f8cb6084b3291004b73e3d414a6d7ba9
Alex Shevkoplyas <alex.fearless.v1.0@gmail.com>
2026-03-24 12:16:39 -0400

    Create 4.1.1 Simple While Loop.py

---
+++ unit-4-programming2/lessons/Lesson 1: While Loops/examples/4.1.1 Simple While Loop.py
@@ -0,0 +1,10 @@
+#First try running this code as is
+#Then, try changing the word "if" to "while" and see how the code runs differently
+
+x = 100
+
+while x > 0:
+  x = x - 20
+  print(x)
+
+print("We are done running the indented code")
