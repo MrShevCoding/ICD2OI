e9dc4134230702bd587bc263365840f178f33e16
Alex Shevkoplyas <alex.fearless.v1.0@gmail.com>
2026-03-24 12:40:14 -0400

    Create problem2.py

---
+++ unit-4-programming2/assignments/U4A2: For Loop Practice/problem2.py
@@ -0,0 +1,10 @@
+# Write a for-loop that sets the table for 10 people
+
+for i in range(1,11):
+    print(f"Moving to table {i}")
+    print("Setting the plate")
+    print("Setting the fork and knife")
+    print("Filling the glass")
+    
+    # In description we need to make a buffer
+    print("")
