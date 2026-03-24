1234ac5ba01d7c8b76131628264fc69b812fb6fd
Alex Shevkoplyas <alex.fearless.v1.0@gmail.com>
2026-03-24 12:17:06 -0400

    Create 4.1.2 Example 1 Game Score.py

---
+++ unit-4-programming2/lessons/Lesson 1: While Loops/examples/4.1.2 Example 1 Game Score.py
@@ -0,0 +1,9 @@
+from time import *
+score = 0
+
+while score < 6000:
+  score = score + 300
+  print("Your score is now", score)
+  sleep(1)
+
+print("You have reached 6,000! Congratulations! You are amazing.")
