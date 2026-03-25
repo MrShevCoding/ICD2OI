c7f5bddbd5c119dc2500fc282d65ea43d7d5baee
Alex Shevkoplyas <alex.fearless.v1.0@gmail.com>
2026-03-25 12:18:00 -0400

    Create randomnumb.py

---
+++ unit-4-programming2/lessons/Lesson 3: Loops and Randomness/Random numbers/randomnumb.py
@@ -0,0 +1,12 @@
+from random import *
+#Make up rules of a simple dice game
+
+for i in range(12):
+  x = randint(1, 6)
+  print("You rolled a " + str(x))
+
+  
+
+
+#Can we play the game repeatedly using a loop?
+# Yes! Using a for loop and randomizing the number each time
