66def67b2ad1e85be21192af85780a844ee2c0e4
Alex Shevkoplyas <alex.fearless.v1.0@gmail.com>
2026-04-13 13:43:57 -0400

    Implement Magic 8 Ball functionality in magic8ball.py

---
+++ unit-2-programming1/assignments/U2 Challenge 4: Magic 8 Ball/magic8ball.py
@@ -0,0 +1,69 @@
+import random
+
+question = str(input("Tell me your question, I shall use the magic 8 B to give! "))
+
+
+question_numb = random.randint(1, 20)
+
+
+if question_numb == 1:
+    print("It is certain")
+    
+if question_numb == 2:
+    print("Reply hazy, try again")
+    
+if question_numb == 3:
+    print("Don't count on it")
+    
+if question_numb == 4:
+    print("It is decidedly so")
+    
+if question_numb == 5:
+    print("Without a doubt")
+
+if question_numb == 6:
+    print("Ask again later")
+    
+if question_numb == 7:
+    print("My reply is no")
+    
+if question_numb == 8:
+    print("Better not tell you now")
+    
+if question_numb == 9:
+    print("My sources say no")
+
+if question_numb == 10:
+    print("Yes definitely")
+    
+if question_numb == 11:
+    print("You may rely on it")
+    
+if question_numb == 12:
+    print("Cannot predict now")
+    
+if question_numb == 13:
+    print("Outlook not so good")
+    
+if question_numb == 14:
+    print("Concentrate and ask again")
+    
+if question_numb == 15:
+    print("Very doubtful")
+    
+if question_numb == 16:
+    print("As I see it, yes")
+    
+if question_numb == 17:
+    print("Most likely")
+    
+if question_numb == 18:
+    print("Outlook good")
+    
+if question_numb == 19:
+    print("Yes")
+    
+if question_numb == 20:
+    print("Signs point to yes")
+    
+#god that was alot of if statements
