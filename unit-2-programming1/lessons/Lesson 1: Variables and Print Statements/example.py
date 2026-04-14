99e836d396a42072f7383b52889185baf14c22b5
Alex Shevkoplyas <alex.fearless.v1.0@gmail.com>
2026-04-14 09:42:38 -0400

    Add example.py with variable and print examples
    Added example variables and print statements demonstrating basic Python syntax.

---
+++ unit-2-programming1/lessons/Lesson 1: Variables and Print Statements/example.py
@@ -0,0 +1,21 @@
+# not mine but an example of variables and prints: https://codehs.com/sandbox/josephwaters/21-variables-and-print
+
+myName = "Joseph"
+x = 8
+animal = "elephants"
+print(myName, "has", x, animal, "and he loves them very much")
+
+
+y = 7
+animal = "snakes"
+print(myName, "has", y, animal)
+
+
+totalPets = x + y
+print(myName, "has", totalPets, "pets")
+
+
+###YOUR TURN###
+#Calculate the difference between x and y and save that result as a
+#new variable named "petsDifference"
+#Then output a statement that tells the user the difference
