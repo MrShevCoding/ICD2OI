bafb78ee3b8d3f7e011584aa389e1dd983a3bbb3
Alex Shevkoplyas <alex.fearless.v1.0@gmail.com>
2026-04-14 09:48:09 -0400

    Create inputStrNumbs.py for input statement examples
    Added a Python script to demonstrate input handling and variable types.

---
+++ unit-2-programming1/lessons/Lesson 2: Input Statements/inputStrNumbs.py
@@ -0,0 +1,25 @@
+##Just Variables##
+
+#Why Doesn't This Work?
+x = 2
+y = "5"
+z = x + y
+
+#Works
+# x = 2
+# y = int("5")
+# z = x + y
+# print(z)
+
+
+
+
+##With User Input##
+
+#Why Doesn't This Work?
+x = 2
+y = input("Enter a number: ")
+z = x + y
+
+###YOUR TURN###
+#Can you rewrite the above code so it works?
