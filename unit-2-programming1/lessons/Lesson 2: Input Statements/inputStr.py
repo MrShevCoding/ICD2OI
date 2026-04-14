58d671cf8148cc9c22d127c9418467222bba85b0
Alex Shevkoplyas <alex.fearless.v1.0@gmail.com>
2026-04-14 09:47:13 -0400

    Create inputStr.py

---
+++ unit-2-programming1/lessons/Lesson 2: Input Statements/inputStr.py
@@ -0,0 +1,23 @@
+#We can use input statements to let the user enter their own values
+#instead of us, the programmers, putting all the values in the program.
+
+#Try running this program.  Enter the values that it asks you for.
+
+#Example 1: Inputing text
+
+firstName = input("Enter your first name: ")
+print("Hey,", firstName, "good to meet you!")
+
+middleInitial= input("Enter your middle initial: ")
+lastName = input("Enter your last name: ")
+
+print("Your full name is", firstName, middleInitial + ".", lastName )
+
+
+
+###YOUR TURN###
+#Write some code that asks the user to input their favourite animal
+#and save what they enter as a variable
+
+#Then, using that variable, write a print statement that tell's them
+#how cool that animal is.
