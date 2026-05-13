c6daa14d737f9ef3e9cac9401a9fbe8e8f49c8aa
Alex Shevkoplyas <alex.fearless.v1.0@gmail.com>
2026-05-13 13:35:45 -0400

    Create 2 Two Object Animation.py

---
+++ unit-8-programming3/lessons/Lesson 4: Linear Animation/2 Two Object Animation.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from time import *
+
+tk = Tk()
+s = Canvas(tk, width=800,height=600, background="yellow")
+s.pack()
+
+#Initialize the ball anchor points
+redBallX1 = 100
+redBallY1 = 100
+
+blueBallX1 = 300
+blueBallY1 = 0
+
+#Loop will run 300 times, each cycle being 1 frame of animation
+for frames in range(300): 
+    
+    #Create the balls using the anchor points
+    redBall = s.create_oval(redBallX1, redBallY1,  redBallX1 + 100,  redBallY1 + 100,  fill="red",  outline="black")
+    blueBall = s.create_oval(blueBallX1, blueBallY1,  blueBallX1 + 100,  blueBallY1 + 100,  fill="blue",  outline="black") 
+
+    #Update to place the balls on the canvas
+    s.update()
+    #Pause Python for a short amount of time
+    sleep(0.0333)
+    #Delete the balls from the canvas
+    s.delete(redBall, blueBall)
+
+    #Update the anchor points so the balls' positions change for next cycle of loop
+    redBallX1 = redBallX1 + 10
+    blueBallY1 = blueBallY1 + 10
+2 Two Object Animation.py
+Displaying 2 Two Object Animation.py.
