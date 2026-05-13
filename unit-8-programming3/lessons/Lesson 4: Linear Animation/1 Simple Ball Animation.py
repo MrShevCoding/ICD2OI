c710808cd6f3ea58f48a0b7aa8eac020453d3b9d
Alex Shevkoplyas <alex.fearless.v1.0@gmail.com>
2026-05-13 13:56:47 -0400

    Update 1 Simple Ball Animation.py

--- unit-8-programming3/lessons/Lesson 4: Linear Animation/1 Simple Ball Animation.py
+++ unit-8-programming3/lessons/Lesson 4: Linear Animation/1 Simple Ball Animation.py
@@ -30,3 +30,39 @@ for frames in range(300):
 
     #Update the anchor points so the ball's x position changes by 10 pixels for next cycle of loop
     ballx1 = ballx1 + 10
+
+
+
+
+
+
+# Below is just the same just for the third task for brick instead
+
+# Comment this below when needed to see the first or vice versa with the upper
+from tkinter import *
+from time import *
+
+tk = Tk()
+s = Canvas(tk, width=800,height=600, background="yellow")
+s.pack()
+
+#Initialize the Rect anchor points
+Rectx1 = 100
+Recty1 = 600
+
+
+#Loop will run 300 times, each cycle being 1 frame of animation
+for frames in range(300): 
+
+    #Create the Rect using the anchor points
+    redRect = s.create_rectangle(Rectx1 - 100,  Recty1,  Rectx1 + 100,  Recty1 + 100,  fill="red",  outline="black") 
+
+    #Update to place the Rect on the canvas
+    s.update()
+    #Pause Python for a short amount of time
+    sleep(0.0399)
+    #Delete the Rect from the canvas
+    s.delete(redRect)
+
+    #Update the anchor points so the Rect's x position changes by 10 pixels for next cycle of loop
+    Recty1 -= 10
