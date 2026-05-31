62f51488bb294982fbfeb2ddaa0274cee038be4b
Alex Shevkoplyas <alex.fearless.v1.0@gmail.com>
2026-05-31 16:25:12 -0400

    Create example2.py

---
+++ unit-2-programming1/lessons/Lesson 4: If, Elif, and Else/example2.py
@@ -0,0 +1,15 @@
+# example 2
+
+likesVG = input("How much do you like video games? ")
+
+if likesVG == "love them":
+    print("Build a gaming setup")
+
+elif likesVG == "like them":
+    print("Play a game on weekends")
+
+elif likeVG == "meh":
+    print("Try a different hobby")
+
+else:
+    print("Maybe read a book instead")
