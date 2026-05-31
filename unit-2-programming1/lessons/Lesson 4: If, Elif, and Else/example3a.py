c29de4bfe7aa60c039042ded6e5c02f1adb2a669
Alex Shevkoplyas <alex.fearless.v1.0@gmail.com>
2026-05-31 16:25:56 -0400

    Create example3a.py

---
+++ unit-2-programming1/lessons/Lesson 4: If, Elif, and Else/example3a.py
@@ -0,0 +1,12 @@
+# example 3a 
+
+studyHours = int(input("How many hours did you study today? "))
+
+if studyHours > 1:
+    print("Nice start!")
+
+if studyHours > 3:
+    print("You're working hard!")
+
+if studyHours > 5:
+    print("Wow, that's a lot of studying!")
