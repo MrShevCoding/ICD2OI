34677e79811b61614c7edf63f7fee42599d3502b
Alex Shevkoplyas <alex.fearless.v1.0@gmail.com>
2026-05-31 16:24:24 -0400

    Create example1.py

---
+++ unit-2-programming1/lessons/Lesson 4: If, Elif, and Else/example1.py
@@ -0,0 +1,10 @@
+# example 1
+
+likesPizza = input("Do you like pizza? ")
+
+if likesPizza == "yes":
+    print("Grab a slice!")
+    print("Enjoy your meal!")
+
+else:
+    print("Maybe try a burger instead.")
